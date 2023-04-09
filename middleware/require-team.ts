38eabdd0dc04757a5bcbad9c818fac9b98f558e6
Artem Tarasenko <shabashab.04@gmail.com>
2023-04-09 10:24:58 +0300

    feat: auto redirect to team

--- middleware/require-team.ts
+++ middleware/require-team.ts
@@ -1,3 +1,10 @@
+import { useAuthStore } from "~/stores/auth.store"
+
 export default defineNuxtRouteMiddleware(async (to, from) => {
-  const user = useSupabaseUser()
+  const authStore = useAuthStore()
+
+  if(!(authStore.profile?.team))
+    return '/team'
+
+  return
 })
