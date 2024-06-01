289fb965b124f867a63075135e2dd9b02ff118a8
Artem Tarasenko <shabashab.04@gmail.com>
2024-06-01 14:15:54 +0300

    fix rickroll

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,11 +17,11 @@ export default defineNuxtConfig({
   supabase: {
     redirect: false,
   },
-  nitro: {
-    routeRules: {
-      '/**': {redirect: { to: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', statusCode: 301 }}
-    }
-  },
+  // nitro: {
+  //   routeRules: {
+  //     '/**': {redirect: { to: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', statusCode: 301 }}
+  //   }
+  // },
   css: [
     'primevue/resources/themes/lara-light-indigo/theme.css',
     'primevue/resources/primevue.min.css',
