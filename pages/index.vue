d39c510cdc5b4a08d3fe84b5b89a47d3c6d420d2
x0lotl <danil200319771@gmail.com>
2025-06-14 11:47:36 +0300

    opeeen

--- pages/index.vue
+++ pages/index.vue
@@ -66,7 +66,7 @@ const maximumPointsFromAnswers = computed(() => {
     <CategorySelector v-model="selectedCategoryLabel"></CategorySelector>
   </div>
   <div class="grid grid-cols-3 grid-rows-[auto] gap-5" v-if="tasksStore.tasks">
-    <!-- <TaskCard :task="task" v-for="task of sortTasksByAnswers(tasksStore.tasks)" /> -->
-      МИ ЩЕ НЕ ПОЧАЛИ
+    <TaskCard :task="task" v-for="task of sortTasksByAnswers(tasksStore.tasks)" />
+      <!-- МИ ЩЕ НЕ ПОЧАЛИ -->
   </div>
 </template>
